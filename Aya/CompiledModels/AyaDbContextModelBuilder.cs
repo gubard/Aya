f78265bda84c55fd4ab6072d382f07783e164b87
gubard <maksimovsergey08@gmail.com>
2026-01-07 12:36:41 +0200

    Update AyaDbContextModelBuilder to modify Guid and improve DateTime comparison logic

--- Aya/CompiledModels/AyaDbContextModelBuilder.cs
+++ Aya/CompiledModels/AyaDbContextModelBuilder.cs
@@ -15,7 +15,7 @@ namespace Aya.CompiledModels
     public partial class AyaDbContextModel
     {
         private AyaDbContextModel()
-            : base(skipDetectChanges: false, modelId: new Guid("be9c274e-0c08-48a9-93ac-570c71be4da6"), entityTypeCount: 3)
+            : base(skipDetectChanges: false, modelId: new Guid("dde11077-c587-462e-b759-15771c7cdc4b"), entityTypeCount: 3)
         {
         }
 
