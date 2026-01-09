6c28d6029ece6960e2bb35341f5448c90a9f36b1
gubard <maksimovsergey08@gmail.com>
2026-01-09 23:36:58 +0200

    Update `SqliteAdo` attribute to include nullable parameter

--- Aya.Contract/AssemblyAttributes.cs
+++ Aya.Contract/AssemblyAttributes.cs
@@ -1,5 +1,5 @@
 ﻿using Aya.Contract.Models;
 using Nestor.Db.Models;
 
-[assembly: SqliteAdo(typeof(FileEntity), nameof(FileEntity.Id))]
+[assembly: SqliteAdo(typeof(FileEntity), nameof(FileEntity.Id), false)]
 [assembly: SourceEntity(typeof(FileEntity), nameof(FileEntity.Id))]
